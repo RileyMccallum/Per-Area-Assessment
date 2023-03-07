8691530bd5a19a435be623918137ba476ba82e67
Matthew Smith <>
2023-03-07 20:10:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+'''
+  Name: 
+  James Hargest College
+  Programming Internal for 2.7 & 2.8 ~ 12 credits
+  Due: 6 April 2023
+  
+  TIP: Use assessment guide to help guide you through this Internal
+'''
+#Basic starting stuff
+import math
+#Variable for storing calculation till the end
+history = []
+
